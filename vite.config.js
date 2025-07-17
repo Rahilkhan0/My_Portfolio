f552ca457f9e794bebda250f84bfd4f4cb6a5c21
Rahil Khan <rahil.apsit.5426@gmail.com>
2025-07-17 18:29:07 +0530

    chnages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/My_Portfolio/', // 👈 IMPORTANT!
   plugins: [react()],
 })
